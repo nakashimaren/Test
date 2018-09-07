08f6a20eafd6fe166749696c9c82b2c86f745a5f
nakashimaren <bookmac87@gmail.com>
2018-09-07 10:48:09 +0900

    //パーティクル管理クラスの生成とプールシステムの実装

---
+++ Test/Assets/PlayParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticle : MonoBehaviour {
+    ParticlePooler particlePooler;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        particlePooler = new ParticlePooler("speed1");
+        Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+    private void Play()
+    {
+        ParticleManager.PlayParticle("speed1", new Vector3(0,100.0f,0));
+    }
+}
---
+++ Test/Assets/Script/Nakashima/ParticlePooler.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+///  パーティクルのプーリング用クラス
+/// </summary>
+public class ParticlePooler : MonoBehaviour
+{
+  
+    public ParticlePooler(string particleName)
+    {
+        this.particleName = particleName;
+    }
+
+    /// パーティクル名
+    private string particleName;
+
+
+    public string ParticleName
+    {
+        get { return particleName; }
+    }
+
+    /// <summary>
+    /// パーティクルを保持しておくリスト
+    /// </summary>
+    private List<ParticleSystem> particleList = new List<ParticleSystem>();
+    public List<ParticleSystem> ParticleList
+    {
+        get { return particleList; }
+    }
+
+    /// <summary>
+    /// 生成元のパーティクル
+    /// </summary>
+    private GameObject particleOrigin = null;
+
+    /// <summary>
+    /// 指定の座標で再生
+    /// </summary>
+    /// <param name="position">Position.</param>
+    public void Play(Vector3 position)
+    {
+        ParticleSystem particle = GetPlayableParticle();
+        if (particle == null)
+        {
+            particle = InstantiateParticle();
+        }
+        particle.transform.position = position;
+        particle.Play();
+    }
+
+    /// <summary>
+    /// 再生可能なパーティクルを取得
+    /// </summary>
+    /// <returns>再生可能なパーティクル.</returns>
+    private ParticleSystem GetPlayableParticle()
+    {
+        return particleList.Where(particle => !particle.isPlaying).FirstOrDefault();
+    }
+
+    /// <summary>
+    /// パーティクル生成
+    /// </summary>
+    /// <returns>The particle.</returns>
+    private ParticleSystem InstantiateParticle()
+    {
+        LoadOrigin();
+        GameObject particleGO = GameObject.Instantiate(particleOrigin) as GameObject;
+        ParticleSystem particle = particleGO.GetComponent<ParticleSystem>();
+        particleList.Add(particle);
+        return particle;
+    }
+
+    /// <summary>
+    /// 生成元のオブジェクトをロード
+    /// </summary>
+    private void LoadOrigin()
+    {
+        if (particleOrigin == null)
+        {
+            string path = "Assets/Resources/Prefab/Particle/"+ particleName+".prefab";
+              particleOrigin = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            particleOrigin.name = particleName;
+        }
+    }
+
+    /// <summary>
+    /// 破棄時処理
+    /// </summary>
+    private void Clean()
+    {
+        particleList.Clear();
+        particleList = null;
+        particleOrigin = null;
+    }
+}

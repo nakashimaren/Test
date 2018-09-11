629ff52f1db0f9a3c5a415272de089e17bc50bbe
nakashimaren <bookmac87@gmail.com>
2018-09-11 14:03:18 +0900

    //車の巡回

--- Test/Assets/Script/Nakashima/CoupleController.cs
+++ Test/Assets/Script/Nakashima/CoupleController.cs
@@ -11,11 +11,19 @@ public class CoupleController : MonoBehaviour {
 
 
     // Use this for initialization
-    void Start ()
+    private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.autoBraking = false;
 
+        //// updateを自動で行わないように設定する
+        //agent.updatePosition = false;
+        //agent.updateRotation = false;
+
+
+        //agent.destination = points[destPoint].position;
+    }
+    void Start ()
+    {
         GotoNextPoint();
     }
     void GotoNextPoint()
@@ -30,6 +38,21 @@ public class CoupleController : MonoBehaviour {
     {
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
+        //// 次の位置への方向を求める
+        //var dir = agent.nextPosition - transform.position;
 
+        //// 方向と現在の前方との角度を計算（スムーズに回転するように係数を掛ける）
+        //float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        //var angle = Mathf.Acos(Vector3.Dot(transform.forward, dir.normalized)) * Mathf.Rad2Deg * smooth;
+
+        //// 回転軸を計算
+        //var axis = Vector3.Cross(transform.forward, dir);
+
+        //// 回転の更新
+        //var rot = Quaternion.AngleAxis(angle, axis);
+        //transform.forward = rot * transform.forward;
+
+        //// 位置の更新
+        //transform.position = agent.nextPosition;
     }
 }

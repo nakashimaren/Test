0d686b3def41da0272de19cc3df7033eb937e445
nakashimaren <bookmac87@gmail.com>
2018-10-30 15:24:23 +0900

    プレふぁぶ

---
+++ Test/Assets/Script/Nakashima/CoupleBlow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoupleBlow : MonoBehaviour
+{
+    public GameObject _player;
+    public string _earthTag;         //除外するタグの名前
+    public float _forceHeight;       //吹き飛ばす高さ調整値
+    public float _forcePower;        //吹き飛ばす強さ調整値
+
+    private void Start()
+    {
+
+    }
+    //何かに触れたときに呼ばれる
+    void OnTriggerEnter(Collider _collider)
+    {
+        //除外対象のタグがついたゲームオブジェクトだったら何もしない
+        if (_earthTag == _collider.tag)
+        {
+            return;
+        }
+        //自分の体は除外
+        if (_player && _player == _collider.gameObject)
+        {
+            return;
+        }
+
+   
+
+
+        //ぶつかった相手からRigitBodyを取り出す
+        Rigidbody otherRigitbody = _collider.GetComponent<Rigidbody>();
+        if (!otherRigitbody)
+        {
+            return;
+        }
+
+        //吹き飛ばす方向を求める(プレイヤーから触れたものの方向)
+        Vector3 toVec = GetAngleVec(_player, _collider.gameObject);
+
+
+        toVec = toVec + new Vector3(0, _forceHeight, 0);
+
+
+        otherRigitbody.AddForce(toVec * _forcePower, ForceMode.Impulse);
+    }
+
+
+    Vector3 GetAngleVec(GameObject _from, GameObject _to)
+    {
+        //高さの概念を入れないベクトルを作る
+        Vector3 fromVec = new Vector3(_from.transform.position.x, 0, _from.transform.position.z);
+        Vector3 toVec = new Vector3(_to.transform.position.x, 0, _to.transform.position.z);
+
+        return Vector3.Normalize(toVec - fromVec);
+    }
+}
+
+
+
---
+++ Test/Assets/Script/Nakashima/CutinAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutinAnim : MonoBehaviour {
+    Animation CutIn;
+    GameObject Canvas;
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Couple")
+        {
+            Canvas.SetActive(true);
+            CutIn.Play();
+        }
+    }
+    void Start () {
+        CutIn = GameObject.Find("Image").gameObject.GetComponent<Animation>();
+        Canvas = GameObject.Find("Canvas");
+        Canvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+}

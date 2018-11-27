6d4b27add150ee929748073dc9a014e14ed37ee9
goldtokoroten <orz_or2_1218@yahoo.co.jp>
2018-11-27 14:14:20 +0900

    コミット

--- Test/Assets/Script/Kanahara/Score.cs
+++ Test/Assets/Script/Kanahara/Score.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    //制限時間
+    public int _score;
+
+    //UI
+    public Image num1;
+    public Image num2;
+    public Image num3;
+    public Image num4;
+
+    public Sprite[] number;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        int score = _score;
+
+        //Debug.Log(score.ToString());
+        
+        int val4 = (int)score / 1000;
+        score -= val4 * 1000;
+
+        int val3 = (int)score / 100;
+        score -= val3 * 100;
+
+        int val2 = (int)score / 10;
+        int val1 = (int)score % 10;
+
+        //Debug.Log(val4.ToString());
+
+        num1.sprite = number[val1];
+        num2.sprite = number[val2];
+        num3.sprite = number[val3];
+        num4.sprite = number[val4];
+
+    }
 }

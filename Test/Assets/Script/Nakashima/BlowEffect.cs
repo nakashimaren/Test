5d65a059dabeed93b8f4637d6634e0267fe7b4a1
nakashimaren <bookmac87@gmail.com>
2018-12-12 09:51:33 +0900

    エフェクトの追加

---
+++ Test/Assets/Script/Nakashima/BlowEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowEffect : MonoBehaviour
+{
+    Vector3 HitPos;
+    GameObject _Couple;
+    bool Hit = false;
+    EffekseerHandle Handle;
+    EffekseerEmitter Burst;
+    // Use this for initialization
+    void Start()
+    {
+        Burst = new EffekseerEmitter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hit)
+        {
+            EffekseerSystem.PlayEffect("Burst2", HitPos);
+
+
+             Hit = false;
+        }
+    }
+    void OnTriggerEnter(Collider _collider)
+    {
+        if (_collider.tag == "Couple")
+        {
+            HitPos = _collider.transform.position;
+            Hit = true;
+        }
+
+    }
+}
+
--- Test/Assets/Script/Nakashima/CoupleDestroy.cs
+++ Test/Assets/Script/Nakashima/CoupleDestroy.cs
@@ -7,7 +7,7 @@ public class CoupleDestroy : MonoBehaviour {
     HitStopCoupleDamage hitStopCoupleDamage;
     EnemyPop enemyPop;
     float span = 3.0f;
-    
+    bool _enabled = false;
     // Use this for initialization
     void Start () {
         hitStopCoupleDamage = GetComponent<HitStopCoupleDamage>();
@@ -17,18 +17,19 @@ public class CoupleDestroy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
 
-	}
+    }
+
+
+
     private IEnumerator Time()
     {
         while (true)
         {
             if (hitStopCoupleDamage.GetFlag())
             {
-                //if (!(GetComponent<Renderer>().isVisible))
-                //{
-                     EffekseerHandle handle = EffekseerSystem.PlayEffect("Burst", transform.position);
-               // }
+ 
                 yield return new WaitForSeconds(span);
                 hitStopCoupleDamage.SetFlag(false);
                 Destroy(transform.parent.gameObject);
